e37645709bac42c734da6566fb25970da008d04d
kukx234 <kukx234@gmail.com>
2019-07-10 18:09:54 +0200

    Added fun to chose role on edit user page, fixed role and vacation model

--- app/Classes/UserRoles.php
+++ app/Classes/UserRoles.php
@@ -3,13 +3,44 @@
 namespace App\Classes;
 
 use Illuminate\Support\Facades\Auth;
+use App\Models\Role;
 
 class UserRoles 
 {
     public static function check()
     {
         foreach (Auth::user()->role as $role) {
-            return $role->id;
+            return $role->role_name;
+        }
+    }
+
+    public static function setAsEmployee()
+    {
+        $roles = Role::all();
+        foreach ($roles as $role) {
+            if($role->role_name === 'Employee'){
+                return $role->id;
+            }
+        }
+    }
+
+    public static function setAsAdmin()
+    {
+        $roles = Role::all();
+        foreach ($roles as $role) {
+            if($role->role_name === 'Admin'){
+                return $role->id;
+            }
+        }
+    }
+
+    public static function setAsApprover()
+    {
+        $roles = Role::all();
+        foreach ($roles as $role) {
+            if($role->role_name === 'Approver'){
+                return $role->id;
+            }
         }
     }
 }
